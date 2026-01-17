bc29c2a7d57b44cd0ff6637c9c85ccf9fa227875
Avinash Malhotra <avimalhotra505@gmail.com>
2026-01-17 10:44:52 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+  /* app.js */
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static("src/public"));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ message: "API is working" });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://127.0.0.1:${PORT}`);
+});
